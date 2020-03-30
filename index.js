b077a8b1644adef8063ceb9e7e21e4124b1cf05a
JerryChenEssilor <jerry.chen@essilor.com.au>
2020-03-30 12:20:15 +1100

    Version 1.0.3 update

--- index.js
+++ index.js
@@ -1,96 +1,58 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { autoUpdater } = require("electron-updater");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
-  mainWindow.loadFile("index.html");
-  mainWindow.on("closed", function() {
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
     mainWindow = null;
   });
-  // mainWindow.once('ready-to-show', () => {
-  //   autoUpdater.checkForUpdatesAndNotify();
-  // });
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
-app.on("ready", () => {
+app.on('ready', () => {
   createWindow();
-  autoUpdater.checkForUpdatesAndNotify();
 });
 
-app.on("window-all-closed", function() {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", function() {
+app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on("app_version", event => {
-  event.csender.send("app_version", { version: app.getVersion() });
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on("update-available", info => {
-  mainWindow.webContents.send("update_available");
-});
-autoUpdater.on("update-downloaded", () => {
-  mainWindow.webContents.send("update_downloaded");
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
 });
 
-ipcMain.on("restart_app", () => {
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+autoUpdater.on("update-not-available", (info) => {
+  mainWindow.webContents.send('update_available');
+});
+
+
+ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
 });
-
-// autoUpdater.on('checking-for-update', () => {
-//   console.log('Checking for update...');
-// })
-// autoUpdater.on('update-available', (info) => {
-//   console.log('Update available.');
-// })
-// autoUpdater.on('update-not-available', (info) => {
-//   console.log('Update not available.');
-// })
-// autoUpdater.on('error', (err) => {
-//   console.log('Error in auto-updater. ' + err);
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-//   let log_message = "Download speed: " + progressObj.bytesPerSecond;
-//   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-//   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-//   console.log(log_message);
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   console.log('Update downloaded');
-// });
-
-
-
-// app.on("ready", function() {
-//   autoUpdater.checkForUpdates();
-// });
-// var message="checking-for-update";
-// autoUpdater.on("checking-for-update", () => {
-//   console.log(message);
-// });
-// autoUpdater.on("update-available", info => {
-//   console.log(info);
-//  // mainWindow.webContents.send("update_available");
-// });
-// autoUpdater.on("update-not-available", info => {
-//   console.log(info);});
-// autoUpdater.on("error", err => {console.log(err);});
-// autoUpdater.on("download-progress", progressObj => {console.log(progressObj);});
-// autoUpdater.on("update-downloaded", info => {
-//   autoUpdater.quitAndInstall();
-// });
